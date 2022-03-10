117a8dd5067a4d100f19955ae3dda65dbc270c45
Nikita Shinov <shinov_96@mail.ru>
2022-03-10 20:30:30 +0300

    adapter implementation

---
+++ CoffeeQuest/Adapters/YLPClient + BusinessSearchClient.swift
@@ -0,0 +1,36 @@
+import MapKit
+import YelpAPI
+
+extension YLPClient: BusinessSearchClient {
+  public func search(with coordinate: CLLocationCoordinate2D,
+                     term: String, limit: UInt, offset: UInt,
+                     success: @escaping (([Business]) -> Void),
+                     failure: @escaping ((Error?) -> Void)) {
+    let yelpCoordinate = YLPCoordinate(latitude: coordinate.latitude,
+                                       longitude: coordinate.longitude)
+    
+    search(with: yelpCoordinate, term: term, limit: limit, offset: offset, sort: .bestMatched) { searchResult, error in
+      guard let searchResult = searchResult, error == nil else {
+        failure(error)
+        return
+      }
+      let businesses = searchResult.businesses.adaptBusinesses()
+      success(businesses)
+    }
+  }
+}
+
+extension Array where Element: YLPBusiness {
+  func adaptBusinesses() -> [Business] {
+    return compactMap { yelpBusiness in
+      guard let yelpCoordinate = yelpBusiness.location.coordinate else {
+        return nil
+      }
+      let coordinate = CLLocationCoordinate2D(latitude: yelpCoordinate.latitude,
+                                              longitude: yelpCoordinate.longitude)
+      return Business(name: yelpBusiness.name,
+                      rating: yelpBusiness.rating,
+                      location: coordinate)
+    }
+  }
+}
--- CoffeeQuest/Controllers/ViewController.swift
+++ CoffeeQuest/Controllers/ViewController.swift
@@ -1,35 +1,3 @@
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import MapKit
 import YelpAPI
 
@@ -37,8 +5,8 @@ public class ViewController: UIViewController {
   
   // MARK: - Properties
   public let annotationFactory = AnnotationFactory()
-  private var businesses: [YLPBusiness] = []
-  private let client = YLPClient(apiKey: YelpAPIKey)
+  private var businesses: [Business] = []
+  public var client: BusinessSearchClient = YLPClient(apiKey: YelpAPIKey)
   private let locationManager = CLLocationManager()
   
   // MARK: - Outlets
@@ -79,38 +47,20 @@ extension ViewController: MKMapViewDelegate {
   }
   
   private func searchForBusinesses() {
-    let coordinate = mapView.userLocation.coordinate
-    guard coordinate.latitude != 0,
-      coordinate.longitude != 0 else {
-        return
-    }
-    
-    let yelpCoordinate = YLPCoordinate(latitude: coordinate.latitude,
-                                       longitude: coordinate.longitude)
-    
-    client.search(with: yelpCoordinate,
-                  term: "coffee",
-                  limit: 35,
-                  offset: 0,
-                  sort: .bestMatched) { [weak self] (searchResult, error) in
-                    guard let self = self else { return }
-                    guard let searchResult = searchResult,
-                      error == nil else {
-                        print("Search failed: \(String(describing: error))")
-                        return
-                    }
-                    self.businesses = searchResult.businesses
-                    DispatchQueue.main.async {
-                      self.addAnnotations()
-                    }
-    }
+    client.search(with: mapView.userLocation.coordinate, term: "coffee", limit: 35, offset: 0, success: {[weak self] businesses in
+      guard let self = self else { return }
+      self.businesses = businesses
+      DispatchQueue.main.async {
+        self.addAnnotations()
+      }
+    }, failure: { error in
+      print ("Search failed: \(String(describing: error))")
+    })
   }
   
   private func addAnnotations() {
     for business in businesses {
-      guard let viewModel = annotationFactory.createBusinessMapView(for: business) else {
-        continue
-      }
+      let viewModel = annotationFactory.createBusinessMapView(for: business)
       mapView.addAnnotation(viewModel)
     }
   }
